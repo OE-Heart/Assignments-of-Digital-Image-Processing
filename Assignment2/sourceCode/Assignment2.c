171413df633f52201093ea18f24fce61cbf16f7e
OE-Heart <leoouyixin@126.com>
2020-11-03 19:43:01 +0800

    realize Binarization

--- Assignment2/sourceCode/Assignment2.c
+++ Assignment2/sourceCode/Assignment2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<windows.h>
 
 void Binarization(char* input, char* output);
@@ -10,10 +11,10 @@ void Closing(char* input, char* output);
 int main()
 {
 	Binarization("pic.bmp", "BinarizationPic.bmp");
-    // Erosion("pic.bmp", "ErosionPic.bmp");
-    // Delation("pic.bmp", "DelationPic.bmp");
-    // Opening("pic.bmp", "OpeningPic.bmp");
-    // Closing("pic.bmp", "ClosingPic.bmp");
+    // Erosion("BinarizationPic.bmp", "ErosionPic.bmp");
+    // Delation("BinarizationPic.bmp", "DelationPic.bmp");
+    // Opening("BinarizationPic.bmp", "OpeningPic.bmp");
+    // Closing("BinarizationPic.bmp", "ClosingPic.bmp");
 
 	return 0;
 }
@@ -48,6 +49,10 @@ void Binarization(char* input, char* output)
     BYTE* data = (BYTE*)malloc(imageSize);  //申请存储图像数据的空间
     fread(data, imageSize, 1, pic1);  //记录图像数据
 
+    int num[256];  //各灰度值的个数
+    double pro[256];  //各灰度值的比例
+    memset(num, 0, sizeof(int)*256);
+
 	int i, j;
 	for (i = 0; i < h; i++)
 	{
@@ -60,10 +65,72 @@ void Binarization(char* input, char* output)
 			int R = data[t+2];
 
 			int Y = 0.299*R + 0.587*G + 0.114*B;
-			if (Y < 128) Y = 0;
-			else Y = 255;
+			if (Y < 0) Y = 0;
+			if (Y > 255) Y = 255;
             
-			data[t] = data[t+1] = data[t+2] = (BYTE)Y;
+            num[Y]++;
+		}
+	}
+
+    for (i = 0; i < 256; i++)
+    {
+        pro[i] = (double)num[i]/h/(w*3-skip)*3;
+    }
+
+    double w1;    //前景像素占比
+    double w2;    //背景像素占比
+    double exp1;  //前景像素灰度值的数学期望
+    double exp2;  //背景像素灰度值的数学期望
+    double u1;    //前景的平均灰度值
+    double u2;    //背景的平均灰度值
+    double g;     //类间方差
+
+    int thresh = 0;
+    double MaxVar = 0;
+    for (int th = 0; th < 256; th++)
+    {
+        w1 = w2 = 0;
+        exp1 = exp2 = 0;
+        u1 = u2 = 0;
+        g = 0;
+        for (i = 0; i < 256; i++)
+        {
+            if (i < th)
+            {
+                w1 += pro[i];
+                exp1 += i*pro[i];
+            }
+            else
+            {
+                w2 += pro[i];
+                exp2 += i*pro[i];
+            }
+        }
+        u1 = exp1/w1;
+        u2 = exp2/w2;
+        g = w1*w2*(u1-u2)*(u1-u2);
+        if (MaxVar < g)
+        {
+            MaxVar = g;
+            thresh = th;
+        }
+    }
+
+    for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w*3-skip; j += 3)
+		{
+			int t = i*bytesPerLine + j;
+
+			int B = data[t];
+			int G = data[t+1];
+			int R = data[t+2];
+
+			int Y = 0.299*R + 0.587*G + 0.114*B;
+			if (Y <= thresh) Y = 0;
+			else Y = 255;
+
+            data[t] = data[t+1] = data[t+2] = (BYTE)Y; 
 		}
 	}
 
@@ -73,7 +140,7 @@ void Binarization(char* input, char* output)
     fwrite(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic2);  //写入位图信息头
     fwrite(data, imageSize, 1, pic2);  //写入图像数据
     fclose(pic2);
-    printf("Binarization suceeded!\n");
+    printf("Binarization suceeded, the thresh is %d!\n", thresh);
     free(data);
 }
 
@@ -107,24 +174,7 @@ void Erosion(char* input, char* output)
     BYTE* data = (BYTE*)malloc(imageSize);  //申请存储图像数据的空间
     fread(data, imageSize, 1, pic1);  //记录图像数据
 
-	// int i, j;
-	// for (i = 0; i < h; i++)
-	// {
-	// 	for (j = 0; j < w*3-skip; j += 3)
-	// 	{
-	// 		int t = i*bytesPerLine + j;
 
-	// 		int B = data[t];
-	// 		int G = data[t+1];
-	// 		int R = data[t+2];
-
-	// 		int Y = 0.299*R + 0.587*G + 0.114*B;
-	// 		if (Y < 128) Y = 0;
-	// 		else Y = 255;
-            
-	// 		data[t] = data[t+1] = data[t+2] = (BYTE)Y;
-	// 	}
-	// }
 
 	FILE* pic2 = fopen(output, "wb");  //创建输出文件
 
@@ -138,15 +188,126 @@ void Erosion(char* input, char* output)
 
 void Delation(char* input, char* output)
 {
+    BITMAPFILEHEADER fileHeader;  //位图文件头
+    BITMAPINFOHEADER infoHeader;  //位图信息头
+    FILE* pic1= fopen(input, "rb");  //打开输入文件
+    if (pic1 == NULL)
+	{
+    	printf("Open file failed!\n");
+    	exit(-1);
+    }
+ 
+    fread(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic1);  //读取文件头
+    fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic1);  //读取信息头
+    WORD bitCount = infoHeader.biBitCount;  //颜色位数
+    if (bitCount != 24)
+    {
+        printf("Only 24 bit color map is allowed!\n");
+    	exit(-1);
+    }
 
+    LONG w = infoHeader.biWidth;    //图像数据的宽度
+    LONG h = infoHeader.biHeight;   //图像数据的高度
+    
+    int bytesPerLine = ((w * bitCount+31)>>5)<<2;  //行数据大小
+    int imageSize = bytesPerLine*h;  //图像数据大小
+    int skip = 4-((w * bitCount)>>3)&3;  //需跳过的字节
+    
+    BYTE* data = (BYTE*)malloc(imageSize);  //申请存储图像数据的空间
+    fread(data, imageSize, 1, pic1);  //记录图像数据
+
+
+
+	FILE* pic2 = fopen(output, "wb");  //创建输出文件
+
+    fwrite(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic2);  //写入位图文件头 
+    fwrite(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic2);  //写入位图信息头
+    fwrite(data, imageSize, 1, pic2);  //写入图像数据
+    fclose(pic2);
+    printf("Delation suceeded!\n");
+    free(data);
 }
 
 void Opening(char* input, char* output)
 {
+    BITMAPFILEHEADER fileHeader;  //位图文件头
+    BITMAPINFOHEADER infoHeader;  //位图信息头
+    FILE* pic1= fopen(input, "rb");  //打开输入文件
+    if (pic1 == NULL)
+	{
+    	printf("Open file failed!\n");
+    	exit(-1);
+    }
+ 
+    fread(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic1);  //读取文件头
+    fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic1);  //读取信息头
+    WORD bitCount = infoHeader.biBitCount;  //颜色位数
+    if (bitCount != 24)
+    {
+        printf("Only 24 bit color map is allowed!\n");
+    	exit(-1);
+    }
 
+    LONG w = infoHeader.biWidth;    //图像数据的宽度
+    LONG h = infoHeader.biHeight;   //图像数据的高度
+    
+    int bytesPerLine = ((w * bitCount+31)>>5)<<2;  //行数据大小
+    int imageSize = bytesPerLine*h;  //图像数据大小
+    int skip = 4-((w * bitCount)>>3)&3;  //需跳过的字节
+    
+    BYTE* data = (BYTE*)malloc(imageSize);  //申请存储图像数据的空间
+    fread(data, imageSize, 1, pic1);  //记录图像数据
+
+
+
+	FILE* pic2 = fopen(output, "wb");  //创建输出文件
+
+    fwrite(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic2);  //写入位图文件头 
+    fwrite(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic2);  //写入位图信息头
+    fwrite(data, imageSize, 1, pic2);  //写入图像数据
+    fclose(pic2);
+    printf("Opening suceeded!\n");
+    free(data);
 }
 
 void Closing(char* input, char* output)
 {
+    BITMAPFILEHEADER fileHeader;  //位图文件头
+    BITMAPINFOHEADER infoHeader;  //位图信息头
+    FILE* pic1= fopen(input, "rb");  //打开输入文件
+    if (pic1 == NULL)
+	{
+    	printf("Open file failed!\n");
+    	exit(-1);
+    }
+ 
+    fread(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic1);  //读取文件头
+    fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic1);  //读取信息头
+    WORD bitCount = infoHeader.biBitCount;  //颜色位数
+    if (bitCount != 24)
+    {
+        printf("Only 24 bit color map is allowed!\n");
+    	exit(-1);
+    }
 
+    LONG w = infoHeader.biWidth;    //图像数据的宽度
+    LONG h = infoHeader.biHeight;   //图像数据的高度
+    
+    int bytesPerLine = ((w * bitCount+31)>>5)<<2;  //行数据大小
+    int imageSize = bytesPerLine*h;  //图像数据大小
+    int skip = 4-((w * bitCount)>>3)&3;  //需跳过的字节
+    
+    BYTE* data = (BYTE*)malloc(imageSize);  //申请存储图像数据的空间
+    fread(data, imageSize, 1, pic1);  //记录图像数据
+
+
+
+	FILE* pic2 = fopen(output, "wb");  //创建输出文件
+
+    fwrite(&fileHeader, sizeof(BITMAPFILEHEADER), 1, pic2);  //写入位图文件头 
+    fwrite(&infoHeader, sizeof(BITMAPINFOHEADER), 1, pic2);  //写入位图信息头
+    fwrite(data, imageSize, 1, pic2);  //写入图像数据
+    fclose(pic2);
+    printf("Closing suceeded!\n");
+    free(data);
 }
